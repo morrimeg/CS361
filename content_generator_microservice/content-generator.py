139b2dadf0cb2d8925d41abd38ccaa71aa8c40b9
morrimeg <morrimeg@oregonstate.edu>
2021-02-04 14:16:35 -0600

    Basic GUI up and running. Was also able to add a dummy textbox.

--- content_generator_microservice/content-generator.py
+++ content_generator_microservice/content-generator.py
@@ -4,6 +4,38 @@
 ## Project: Content Generator Microservice
 ## Description:
 
-from tkinter import *
+import tkinter as tk
 
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+        self.primary_keyword_text_box()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+    def primary_keyword_text_box(self):
+        pk = "primary word"
+        self.primary_keyword = tk.Entry(self, width=7, textvariable=pk)
+        self.primary_keyword.pack(side="bottom")
+
+
+root = tk.Tk()
+root.title("Content Generator")
+root.geometry('800x400')
+app = Application(master=root)
+app.mainloop()
 
